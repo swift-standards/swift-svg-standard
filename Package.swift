721c76b68b3d05048af1d9c0c12cacf6fd2e62c0
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-10 01:18:02 +0100

    Apply swift-format and lint fixes

--- Package.swift
+++ Package.swift
@@ -5,10 +5,11 @@ import PackageDescription
 let package = Package(
     name: "swift-svg-standard",
     platforms: [
-        .macOS(.v15),
-        .iOS(.v18),
-        .tvOS(.v18),
-        .watchOS(.v11),
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26),
+        .visionOS(.v26),
     ],
     products: [
         .library(
@@ -17,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-w3c-svg", from: "0.1.0")
+        .package(url: "https://github.com/swift-standards/swift-w3c-svg", from: "0.1.0"),
     ],
     targets: [
         .target(
